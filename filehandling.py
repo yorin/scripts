38a1a2b14e6a41d9cc063aff3895b4fa15789cbd
yorin <werbyvillarino@gmail.com>
2015-03-14 06:56:07 +0800

    added filehandling.py

--- filehandling.py
+++ filehandling.py
@@ -4,12 +4,13 @@
 fo = open("foo.txt", "r+")
 #read the fifth 
 str = fo.read(5);
+print "Read String is : ", str
 
 #open a file
 fo2 = open("foo.txt", "r+")
 #read all
 str2 = fo2.read();
-print "Read String is : ", str
 print "Read all String: ", str2
+
 # Close opend file
 fo.close()
