ae817b7e48464d06a5863cc96c3ef0f8b570fbfd
yorin <werbyvillarino@gmail.com>
2015-03-13 17:50:01 +0800

    added diffence modified print out to addition helloworld.py

--- hellworld.py
+++ hellworld.py
@@ -6,4 +6,6 @@ varb = 20
 print "hello world!"
 
 sum = vara + varb
-print "var a + var b = %d" % sum
+difference = varb - vara
+print "sum of var a + var b = %d" % sum
+print "differece of var b - var a = %d" %difference
