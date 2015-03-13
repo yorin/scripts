62775c9932862e227e6d46493ec157f66246c95b
yorin <werbyvillarino@gmail.com>
2015-03-14 04:37:08 +0800

    added math function, squareroot with print out

--- hellworld.py
+++ hellworld.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import math
 
 vara = 10
 varb = 20
@@ -12,6 +13,8 @@ quotient = varb / vara
 power = vara ** varb
 root = varb ** (1.0/vara)
 modulo = varb % vara
+squareroota = math.sqrt(vara)
+squarerootb = math.sqrt(varb)
 print "sum of var a + var b = %d" % sum
 print "differece of var b - var a = %d" %difference
 print "product of var a * var b =  %d" %product
@@ -19,3 +22,4 @@ print "quotient of var b / var a =  %d" %quotient
 print "power of var a exponent var b =  %d" %power
 print "root of radicand var b degree var a =  %s" %root
 print "var b mod var a =  %s" %modulo
+print "square root of var a: %s square root of var b: %s" %(squareroota,squarerootb)
