8567784ca445c508dc84d3895294986132b9a8ba
yorin <werbyvillarino@gmail.com>
2015-03-13 17:58:39 +0800

    added product with print out

--- hellworld.py
+++ hellworld.py
@@ -7,5 +7,7 @@ print "hello world!"
 
 sum = vara + varb
 difference = varb - vara
+product = vara * varb
 print "sum of var a + var b = %d" % sum
 print "differece of var b - var a = %d" %difference
+print "product of var a * var b =  %d" %product
