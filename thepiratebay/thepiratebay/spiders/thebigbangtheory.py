b915e4269e46d62aff865dff874a684737d3b842
yorin <werbyvillarino@gmail.com>
2015-03-16 03:45:34 +0800

    modified scrapy output test

--- thepiratebay/thepiratebay/spiders/thebigbangtheory.py
+++ thepiratebay/thepiratebay/spiders/thebigbangtheory.py
@@ -4,16 +4,32 @@ from thepiratebay.items import ThepiratebayItem
 
 class ThebigbangtheorySpider(scrapy.Spider):
     name = "thebigbangtheory"
-    allowed_domains = ["thepiratebay.se/tv/9386/"]
+    allowed_domains = ["thepiratebay.se"]
     start_urls = (
-        'https://www.thepiratebay.se/tv/9386//',
+        'https://www.thepiratebay.se/tv/9386/',
     )
 
     def parse(self, response):
-        torrent = ThepiratebayItem()
+        #torrent = ThepiratebayItem()
         #torrent['url'] = response.url
-        torrent['name'] = response.xpath("//dd/span/a/text()").extract()
-        torrent['url'] = response.xpath("//dd/span/a/@href").extract()
+        #first method
+        #torrent['name'] = response.xpath("//dd/span/a/text()").extract()
+        #torrent['url'] = response.xpath("//dd/span/a/@href").extract()
+        #another method
+        #torrent = response.xpath('//dd/span/a[contains(@href, text())]')
+        #torrent.extract()
+        # or
+        #torrent = response.xpath("//dd/span/a[contains(@href)]/text()").extract()
         #items = []
-        return torrent
+        #torrent.append(torrent)
+        #return torrent
         #pass
+        torrentfiles = response.xpath("//dd/span")
+        torrents = []
+        for torrentfiles in torrentfiles:
+            torrent = ThepiratebayItem()
+            torrent ["name"] = torrentfiles.xpath("a/text()").extract()
+            torrent ["url"] = torrentfiles.xpath("a/@href").extract()
+            torrents.append(torrent)
+        return torrents
+
