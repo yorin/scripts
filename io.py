0e57e7137d63644168fdd074771808eb1bbd2b11
yorin <werbyvillarino@gmail.com>
2015-03-20 19:41:21 +0800

    removed io.py

--- io.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-#accepts and displays string
-var1 = raw_input("Enter your input: ");
-print "Received input is : ",  var1
-
-#accepts only integers or numbers
-var2 = input("Enter your 2nd input: ");
-print "Received input is : ",  var2
