e24da006fbc3d41add1d7537803305ccd3ad6af7
yorin <werbyvillarino@gmail.com>
2015-03-22 10:46:25 +0800

    3/22/2015

--- exampleurllib.py
+++ exampleurllib.py
@@ -1,6 +1,16 @@
 import urllib2
+import re
+import json
 
-req = 'http://www.voidspace.org.uk'
+req ='http://www.google.com/finance/converter?a=1&from=USD&to=PHP'
+#req ='http://www.google.com/finance/converter?a=1&from=USD&to=PHP?format=json'
+#req = 'http://www.voidspace.org.uk'
 response = urllib2.urlopen(req)
-print response.read()
+#print response.read()
 #the_page = response.read()
+raw_data = response.read()
+#raw_data = raw_data.replace('\xa0','')
+#j = req.sanitize(raw_data)
+#data = json.loads(j)
+#print raw_data
+print re.search("<div id=currency_converter_result>.*", raw_data).group()
