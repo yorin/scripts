4763ed94c039c56afaa0daa6a2c1ef73f8b6335f
yorin <werbyvillarino@gmail.com>
2015-03-22 12:03:07 +0800

    3/22/2015

--- exampleurllib.py
+++ exampleurllib.py
@@ -1,6 +1,5 @@
 import urllib2
 import re
-import json
 #http://stackoverflow.com/questions/3398852/using-python-remove-html-tags-formatting-from-a-string
 #http://stackoverflow.com/questions/3272123/python-regex-search-multiple-values-in-one-string
 
@@ -19,4 +18,8 @@ print re.search("<div id=currency_converter_result>.*", raw_data).group()
 raw_data = re.search("<div id=currency_converter_result>.*", raw_data).group()
 p = re.compile(r'<.*?>|=')
 print p.sub( '', raw_data)
+#raw_date = p.sub('', raw_data)
+print re.split("\s+", p.sub('', raw_data))
+list1 = re.split("\s+", p.sub('', raw_data))
 
+print list1[2]
