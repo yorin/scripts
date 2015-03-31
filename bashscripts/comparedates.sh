467e9b8677f684f6d9b6501e8b6beb28b850d45d
yorin <werbyvillarino@gmail.com>
2015-04-01 00:06:17 +0800

    compare dates

---
+++ bashscripts/comparedates.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+todate=$(date -d 2013-07-18 +"%y%m%d")
+cond=$(date -d 2013-07-15 +"%y%m%d")
+
+if [ $todate -ge $cond ]; #put the loop where you need it
+then
+ echo 'yes';
+ sleep 3;
+fi
+
+date_a=2013-07-18
+date_b=2013-07-15
+
+if [[ "$date_a" > "$date_b" ]] ;
+then
+    echo "break"
+fi
