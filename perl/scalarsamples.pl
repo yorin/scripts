48bbe05469eee3c5f8d0395812575f7f9ca863c9
yorin <werbyvillarino@gmail.com>
2015-04-02 20:42:05 +0800

    4/2/2015

---
+++ perl/scalarsamples.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+$age = 25;             # An integer assignment
+$name = "John Paul";   # A string 
+$salary = 1445.50;     # A floating point
+
+print "Age = $age\n";
+print "Name = $name\n";
+print "Salary = $salary\n";
