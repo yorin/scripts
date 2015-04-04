02a0f2b5a296d9368840440ddcc54a2325bb455d
yorin <werbyvillarino@gmail.com>
2015-04-05 00:58:06 +0800

    4/5/2015

--- bashscripts/30minupdate.sh
+++ bashscripts/30minupdate.sh
@@ -2,9 +2,19 @@
 #env stat -c %Y 30minupdate.sh
 #1428164696
 #604800 7days
+COMP="604800"
 CURFILEDATE=$(env stat -c %Y datewatch.sh)
 SERVERDATE=$(env date +%s)
 echo "CURFILEDATE: $CURFILEDATE"
 echo "SERVERDATE: $SERVERDATE"
+echo "COMP: $COMP"
 SUMDIFF=$((SERVERDATE - CURFILEDATE))
 echo "SUMDIFF: $SUMDIFF"
+if [ "$COMP" -le "$SUMDIFF" ]
+then
+    echo "YAHOO"
+else
+    echo "NOPE"
+
+fi
+exit 0
