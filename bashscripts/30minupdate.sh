19eb37eda165a4eaae50dad3a8c9cf9a3b49c4bb
yorin <werbyvillarino@gmail.com>
2015-04-05 00:45:59 +0800

    04/05/2015

--- bashscripts/30minupdate.sh
+++ bashscripts/30minupdate.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #env stat -c %Y 30minupdate.sh
 #1428164696
-CURDATEFILE=$(env stat -c %Y datewatch.sh)
+#604800 7days
+CURFILEDATE=$(env stat -c %Y datewatch.sh)
 SERVERDATE=$(env date +%s)
-echo "$CURDATEFILE"
-echo "$SERVERDATE"
-env TZ=EDT date +%s
-DATA=$((1428157831 - 1428157802))
-#1428164696
-echo "$DATA"
+echo "CURFILEDATE: $CURFILEDATE"
+echo "SERVERDATE: $SERVERDATE"
+SUMDIFF=$((SERVERDATE - CURFILEDATE))
+echo "SUMDIFF: $SUMDIFF"
