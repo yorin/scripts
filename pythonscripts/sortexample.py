08440ebca64355bc568e832b5e0c5585deefa4c8
yorin <werbyvillarino@gmail.com>
2015-04-07 23:36:34 +0800

    04/07/2015

---
+++ pythonscripts/sortexample.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+aList = [123, 'xyz', 'zara', 'abc', 'xyz'];
+
+aList.sort();
+print "List ascending: ", aList;
+
+aList.sort(reverse=True);
+print "List descending: ", aList;
+
+
