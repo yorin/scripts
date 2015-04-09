2292e19247f46993bbb61be8ffe720af0eac22ed
yorin <werbyvillarino@gmail.com>
2015-04-09 17:17:38 +0800

    04/09/2015

--- perl/sortexample.pl
+++ perl/sortexample.pl
@@ -1,3 +1,13 @@
+# standard sorting
 @theList = (4, 1, 2, 6, 93, 2, 65);
 @theSortedList = sort @theList;
+print join(', ', @theSortedList)."\n";
+
+#custom sorting
+@theList = (4, 1, 2, 6, 93, 2, 65);
+@theSortedList = sort {$b <=> $a} @theList;
+print join(', ', @theSortedList)."\n";
+
+@theList = (3, "pear", "four", "apple", 7);
+@theSortedList = sort {$b cmp $a} @theList;
 print join(', ', @theSortedList)."\n";
