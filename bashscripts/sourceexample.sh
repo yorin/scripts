4dea5e8020e83312c57e638d9a581cc5bfee86b1
yorin <werbyvillarino@gmail.com>
2015-04-16 07:07:05 +0800

    ț04/16/2015ț

--- bashscripts/sourceexample.sh
+++ bashscripts/sourceexample.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#http://www.daveeddy.com/2010/09/20/import--source-files-in-bash/
+#http://stackoverflow.com/questions/5228345/bash-script-how-to-reference-a-file-for-variables
 source File1.txt
 echo $NAME
 echo $DATETODAY
