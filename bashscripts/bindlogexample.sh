d4c84ac4c90932fef4243770fab1eda276e7a2b9
yorin <werbyvillarino@gmail.com>
2015-04-18 12:50:53 +0800

    04/18/2015

--- bashscripts/bindlogexample.sh
+++ bashscripts/bindlogexample.sh
@@ -1,1 +1,3 @@
+#https://raymii.org/s/software/Bind-GNUPlot-DNS-Bar-Graph.html
+#http://www.pablumfication.co.uk/2010/09/03/bind-logs-top-dns-queries/
  awk '{ print $8 }' /var/named/data/named.run | sort | uniq -c | sort -n | tail -n 100
