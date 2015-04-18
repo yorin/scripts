ebe040f462c547ad5c3a608534fc7c9a9b82daed
yorin <werbyvillarino@gmail.com>
2015-04-18 12:52:47 +0800

    04/18/2015

--- bashscripts/bindlogexample.sh
+++ bashscripts/bindlogexample.sh
@@ -1,3 +1,4 @@
 #https://raymii.org/s/software/Bind-GNUPlot-DNS-Bar-Graph.html
 #http://www.pablumfication.co.uk/2010/09/03/bind-logs-top-dns-queries/
- awk '{ print $8 }' /var/named/data/named.run | sort | uniq -c | sort -n | tail -n 100
+#https://encodable.com/tech/blog/2008/12/17/Count_IP_Addresses_in_Access_Log_File_BASH_OneLiner
+awk '{ print $8 }' /var/named/data/named.run | sort | uniq -c | sort -n | tail -n 100
