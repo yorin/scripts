2b3d05a75cb069ba03013e23254c6ec85f5329e1
yorin <werbyvillarino@gmail.com>
2015-04-20 17:30:05 +0800

    04/20/2015

--- bashscripts/sedexample.sh
+++ bashscripts/sedexample.sh
@@ -1,1 +1,15 @@
 sed -e '1d' -e '2d' -e '5d' books.txt 
+
+sed -n '
+h;n;H;x
+s/\n/, /
+/Paulo/!b Print
+s/^/- /
+:Print
+p' books.txt
+
+sed -n 'h;n;H;x;s/\n/, /;/Paulo/!b Print; s/^/- /; :Print;p' books.txt 
+
+sed -n 'h;n;H;x; s/\n/, /; :Loop;/Paulo/s/^/-/; /----/!t Loop; p' books.txt 
+
+sed 'p' books.txt
